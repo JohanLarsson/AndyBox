e01a232c9b1ef133fcc7449a8eef9366c5b8ac9f
Johan Larsson <johan_gavle1@hotmail.com>
2016-03-26 00:47:02 +0100

    Initial

---
+++ AndyBox/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AndyBox
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Andy");
+            Console.ReadKey();
+        }
+    }
+}
